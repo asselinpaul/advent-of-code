4bd66085ba936736cb08f3f136228ca7245e3f1e
Paul Asselin <mail@asselinpaul.com>
2021-12-05 22:01:28 -0500

    day 1

---
+++ 1/solution.nim
@@ -0,0 +1,15 @@
+import strutils
+import sequtils
+
+let contents = readFile("input.txt") 
+let str_numbers = contents.splitLines()
+let numbers = map(str_numbers, proc(x: string): int = parseInt(x))
+
+var acc = 0
+
+for idx, _ in numbers.pairs:
+    if idx == 0: continue
+    if numbers[idx] > numbers[idx-1]:
+        acc += 1
+
+echo acc
